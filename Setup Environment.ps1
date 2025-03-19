8c540bba5b14bc9a24c15b4e6ed99cb4afc6b311
spudboydan <lanepitman@gmail.com>
2025-03-19 03:29:58 -0500

    Added new function, Get-AteraAgents, to provide a 'quick and dirty' way to retrieve every Atera endpoint so that they may be easily searched for with any number of conditions

--- Setup Environment.ps1
+++ Setup Environment.ps1
@@ -4,6 +4,7 @@
 # ================================================================================
 # Functions
 #	Download-AdultSwim
+#	Get-AteraAgents
 # 	Get-Duplicates
 # 	Get-DuplicatesFast
 # 	Get-DuplicatesV2
@@ -31,6 +32,44 @@ function Download-AdultSwim
 			$PSCmdlet.ThrowTerminatingError($PSItem)
 		}
 	}
+
+function Get-AteraAgents
+{
+	param ([parameter(Mandatory = $true, Position = 0)]
+		[string]$Key,
+		[parameter(Mandatory = $false, Position = 1)]
+		[int]$PageNumber = 1,
+		[parameter(Mandatory = $false, Position = 2)]
+		[int]$ItemAmount = 20,
+		[parameter(Mandatory = $false, Position = 3)]
+		[switch]$All)
+
+	switch ($All) {
+		$true	{[int]$Pages = (Invoke-RestMethod -Uri "https://app.atera.com/api/v3/agents?page=1&itemsInPage=50" -Headers @{
+						"method"="GET"
+						"scheme"="https"
+  						"accept"="application/json;charset=utf-8,*/*"
+  						"accept-encoding"="gzip, deflate, br, zstd"
+  						"accept-language"="en-US,en;q=0.9"
+  						"x-api-key"="$Key"}).totalPages;
+				for ($i = 1; $i -le $Pages; $i++) {
+					(Invoke-RestMethod -Uri "https://app.atera.com/api/v3/agents?page=$i&itemsInPage=50" -Headers @{
+						"method"="GET"
+						"scheme"="https"
+  						"accept"="application/json;charset=utf-8,*/*"
+  						"accept-encoding"="gzip, deflate, br, zstd"
+  						"accept-language"="en-US,en;q=0.9"
+  						"x-api-key"="$Key"}).items}}
+
+		$false	{(Invoke-RestMethod -Uri "https://app.atera.com/api/v3/agents?page=$PageNumber&itemsInPage=$ItemAmount" -Headers @{
+				"method"="GET"
+				"scheme"="https"
+  				"accept"="application/json;charset=utf-8,*/*"
+  				"accept-encoding"="gzip, deflate, br, zstd"
+  				"accept-language"="en-US,en;q=0.9"
+  				"x-api-key"="$Key"}).items}
+		      }
+}
 
 function Get-Duplicates 
 	{
@@ -100,7 +139,7 @@ function Invoke-AteraApi
 		[string]$Key,
 		[parameter(Mandatory = $true, Position = 1)]
 		[ValidateSet("Agents", "Customers", "CustomerAgents", "CustomerById", "MachineByName")]
-		[string]$QueryOf,
+		[string]$Query,
 		[parameter(Mandatory = $false, Position = 2)]
 		[long]$CustomerId,
 		[parameter(Mandatory = $false, Position = 3)]
@@ -110,24 +149,8 @@ function Invoke-AteraApi
 		[parameter(Mandatory = $false, Position = 5)]
 		[int]$ItemAmount = 20)
 
-switch ($QueryOf) {
-	"Agents" 	{Invoke-RestMethod -Uri "https://app.atera.com/api/v3/agents?page=$PageNumber&itemsInPage=$ItemAmount" -Headers @{
-				"method"="GET"
-				"scheme"="https"
-  				"accept"="application/json;charset=utf-8,*/*"
-  				"accept-encoding"="gzip, deflate, br, zstd"
-  				"accept-language"="en-US,en;q=0.9"
-  				"x-api-key"="$Key"}}
-
-	"Customers"	{Invoke-RestMethod -Uri "https://app.atera.com/api/v3/customers?page=$PageNumber&itemsInPage=$ItemAmount" -Headers @{
-				"method"="GET"
-				"scheme"="https"
-  				"accept"="application/json;charset=utf-8,*/*"
-  				"accept-encoding"="gzip, deflate, br, zstd"
-  				"accept-language"="en-US,en;q=0.9"
-  				"x-api-key"="$Key"}}
-
-	"CustomerAgents"	{Invoke-RestMethod -Uri "https://app.atera.com/api/v3/agents/customer/$CustomerId`?page=$PageNumber&itemsInPage=$ItemAmount" -Headers @{
+	switch ($Query) {
+		"Agents" 	{Invoke-RestMethod -Uri "https://app.atera.com/api/v3/agents?page=$PageNumber&itemsInPage=$ItemAmount" -Headers @{
 					"method"="GET"
 					"scheme"="https"
   					"accept"="application/json;charset=utf-8,*/*"
@@ -135,24 +158,40 @@ switch ($QueryOf) {
   					"accept-language"="en-US,en;q=0.9"
   					"x-api-key"="$Key"}}
 
-	"CustomerById"	{Invoke-RestMethod -Uri "https://app.atera.com/api/v3/customers/$CustomerId" -Headers @{
-				"method"="GET"
-				"scheme"="https"
-  				"accept"="application/json;charset=utf-8,*/*"
-  				"accept-encoding"="gzip, deflate, br, zstd"
-  				"accept-language"="en-US,en;q=0.9"
-  				"x-api-key"="$Key"}}
+		"Customers"	{Invoke-RestMethod -Uri "https://app.atera.com/api/v3/customers?page=$PageNumber&itemsInPage=$ItemAmount" -Headers @{
+					"method"="GET"
+					"scheme"="https"
+  					"accept"="application/json;charset=utf-8,*/*"
+  					"accept-encoding"="gzip, deflate, br, zstd"
+  					"accept-language"="en-US,en;q=0.9"
+  					"x-api-key"="$Key"}}
 
-	"MachineByName"	{Invoke-RestMethod -Uri "https://app.atera.com/api/v3/agents/machine/$MachineName`?page=$PageNumber&itemsInPage=$ItemAmount" -Headers @{
-				"method"="GET"
-				"scheme"="https"
-  				"accept"="application/json;charset=utf-8,*/*"
-  				"accept-encoding"="gzip, deflate, br, zstd"
-  				"accept-language"="en-US,en;q=0.9"
-  				"x-api-key"="$Key"}}
-		  }
+		"CustomerAgents"	{Invoke-RestMethod -Uri "https://app.atera.com/api/v3/agents/customer/$CustomerId`?page=$PageNumber&itemsInPage=$ItemAmount" -Headers @{
+						"method"="GET"
+						"scheme"="https"
+  						"accept"="application/json;charset=utf-8,*/*"
+  						"accept-encoding"="gzip, deflate, br, zstd"
+  						"accept-language"="en-US,en;q=0.9"
+  						"x-api-key"="$Key"}}
+
+		"CustomerById"	{Invoke-RestMethod -Uri "https://app.atera.com/api/v3/customers/$CustomerId" -Headers @{
+					"method"="GET"
+					"scheme"="https"
+  					"accept"="application/json;charset=utf-8,*/*"
+  					"accept-encoding"="gzip, deflate, br, zstd"
+  					"accept-language"="en-US,en;q=0.9"
+  					"x-api-key"="$Key"}}
+
+		"MachineByName"	{Invoke-RestMethod -Uri "https://app.atera.com/api/v3/agents/machine/$MachineName`?page=$PageNumber&itemsInPage=$ItemAmount" -Headers @{
+					"method"="GET"
+					"scheme"="https"
+  					"accept"="application/json;charset=utf-8,*/*"
+  					"accept-encoding"="gzip, deflate, br, zstd"
+  					"accept-language"="en-US,en;q=0.9"
+  					"x-api-key"="$Key"}}
+		  	}
 }
-
+	
 function Play-Song
 	{
 		[Console]::Beep(466.16,300);
