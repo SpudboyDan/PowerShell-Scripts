42da668e85fd83fa967acf662f35d323b7bf4347
SpudboyDan <lanepitman@gmail.com>
2025-08-06 17:40:17 -0500

    Added CSharp code to repository

---
+++ CSharp/CopyFiles/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleUI
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string rootPath = @"C:\Users\Craig\Documents\PowerShell\Notes";
+			string[] files = Directory.GetFiles(rootPath); 
+			string destinationFolder = @"C:\Temp\";
+			foreach (string file in files)
+			{
+				Console.WriteLine(file);
+				File.Copy(file, $"{destinationFolder}{ Path.GetFileName(file) }", true);
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
---
+++ CSharp/FileSystem/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleUI
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string rootPath = @"C:\Users\Craig\Documents\PowerShell";
+
+			// string[] directories = Directory.GetDirectories(rootPath, "*", SearchOption.AllDirectories);
+
+			// foreach (string dir in directories)
+			//{
+			//	Console.WriteLine(dir);
+			//}
+
+			//var files = Directory.GetFiles(rootPath, "*.*", SearchOption.AllDirectories);
+
+			//foreach (string file in files)
+			//{
+				//Console.WriteLine(file);
+				//Console.WriteLine(Path.GetFileName(file));
+				//Console.WriteLine(Path.GetFileNameWithoutExtension(file));
+				//Console.WriteLine(Path.GetDirectoryName(file));
+				//var info = new FileInfo(file);
+				//Console.WriteLine($"{ Path.GetFileName(file) }: { info.Length } bytes");
+			//}
+
+			//bool directoryExists = Directory.Exists(@"C:\Users\Craig\Documents\PowerShell\SubFolderC");
+			//string newPath = @"C:\Users\Craig\Documents\PowerShell\SubFolderC\SubSubFolderD";
+
+			//if (directoryExists)
+			//{
+			//	Console.WriteLine("The directory exists");
+			//}
+			//else
+			//{
+			//	Console.WriteLine("The directory does not exist");
+			//	Directory.CreateDirectory(newPath);
+			//}
+
+			Console.ReadLine();
+		}
+	}
+}
