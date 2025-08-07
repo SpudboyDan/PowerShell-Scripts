5a9c2585fbbd45196113f9a8873b9e8625f81854
SpudboyDan <lanepitman@gmail.com>
2025-08-07 16:56:48 -0500

    Created new demo C Sharp project

---
+++ CSharp/FindDuplicateFiles/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ConsoleUI
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string rootPath = @"C:\Users\Craig\Documents\PowerShell";
+			string[] dirs = Directory.GetDirectories(rootPath, "*", SearchOption.AllDirectories);
+
+			foreach (string dir in dirs)
+			{
+				Console.WriteLine(dir);
+			}
+			Console.ReadLine();
+		}
+	}
+}
