5d3c03dbdc806b63b16fc4d0be60803731182ce1
SpudboyDan <lanepitman@gmail.com>
2025-09-09 11:30:35 -0500

    Added notes for FFmpeg, removed old/obsolete scripts, edited script to remove any PII

--- Scripts/Drafts/Splashtop Registry Check v3.ps1
+++ Scripts/Drafts/Splashtop Registry Check v3.ps1
@@ -4,7 +4,7 @@ $encryptionKey = {[key]};
 
 $secureString = ConvertTo-SecureString -String $encryptedSecureString -Key $encryptionKey;
 
-$secureCred = New-Object System.Management.Automation.PSCredential('metrotech', $secureString);
+$secureCred = New-Object System.Management.Automation.PSCredential('SMTPName', $secureString);
 
 $screenLockReg = (Get-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Splashtop Inc.\Splashtop Remote Server").ScreenLock;
 
@@ -14,7 +14,7 @@ $PSEmailServer = 'mail.smtp2go.com';
 
 switch ($screenLockReg)
 {
-0 {Send-MailMessage -To 'lane@metro-tech.net' -Subject 'Splashtop registry setting' -Body "WARNING: The Splashtop Streamer settings for $localUsers on computer $env:COMPUTERNAME are not configured to automatically lock the computer at the end of a session. Splashtop settings are now updating to correct this." -Cc 'craig@metro-tech.net' -From 'backups@metro-tech.net' -Credential $secureCred -Port 587}
+0 {Send-MailMessage -To 'name@company.com' -Subject 'Splashtop registry setting' -Body "WARNING: The Splashtop Streamer settings for $localUsers on computer $env:COMPUTERNAME are not configured to automatically lock the computer at the end of a session. Splashtop settings are now updating to correct this." -Cc 'name@company.com' -From 'name@company.com' -Credential $secureCred -Port 587}
 0 {Set-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Splashtop Inc.\Splashtop Remote Server" -Name ScreenLock -Type Dword -Value 00000001}
 0 {Restart-Service 'SplashtopRemoteService'}
 }
