d9b0f1a08282cf4f82ce4cb960f53f0c98cc8131
SpudboyDan <lanepitman@gmail.com>
2025-09-17 14:31:46 -0500

    Re-added new exercises after failed merge

---
+++ C/CharCount.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+<<<<<<< HEAD
+/* count characters in input; 1st version */
+=======
+
+/*count characters in input; version 1*/
+>>>>>>> ac073688527cb0580a222d92997fb89a4c31dc8d
+
+int main()
+{
+	long nc;
+<<<<<<< HEAD
+
+	nc = 0;
+	while (getchar() != EOF)
+		++nc;
+	printf("%ld\n", nc);
+=======
+	nc = 0;
+	while (getchar() != EOF)
+		++nc;
+	printf("%1ld\n", nc);
+>>>>>>> ac073688527cb0580a222d92997fb89a4c31dc8d
+}
