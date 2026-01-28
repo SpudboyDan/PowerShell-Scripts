34cd1d2f86d827fbb5383ff1feaddb61c0777cfc
SpudboyDan <lanepitman@gmail.com>
2026-01-27 19:30:32 -0600

    Added new exercises from "Python Crash Course"

---
+++ Python/exercise_4-1.py
@@ -0,0 +1,6 @@
+favorite_pizzas = ['pepperoni', 'meat lovers', 'hawaiian']
+
+for pizza in favorite_pizzas:
+    print(f"I like {pizza.title()} pizza")
+
+print("\nI like pizza a fair bit")
---
+++ Python/exercise_4-2.py
@@ -0,0 +1,6 @@
+animals = ['platypus', 'pangolin', 'dolphin']
+
+for animal in animals:
+    print(f"The {animal.title()} is an interesting creature.")
+
+print("\nAll of these animals are mammals!")
